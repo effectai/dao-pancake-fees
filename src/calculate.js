6a00e1734558e2204615d61bec0da83c0c230fd9
David Britt <david@effect.ai>
2021-07-30 13:19:10 +0200

    Use BN for arithimetic operations

--- src/calculate.js
+++ src/calculate.js
@@ -1,17 +1,21 @@
 const Web3 = require('web3')
 const BN = Web3.utils.BN
 
+const formatFee = (fee) => Web3.utils.fromWei(fee)
+const buildList = (data) => data.map(tx => feeObject(tx))
+
 /**
  * Calculate the fee for a transaction and build a fee object
  * @param {Object} tx - transaction object
  * @param {number} efxPcsLpRatio - EFX/WBNB fee ratio, default is 0.98, must be a number between 0 and 1
  * @return {Object} fee object
  */
-const feeObject = (tx, efxPcsLpRatio = 0.98) => {
-    const totalFee = 0.0025;
-    const lpFee = 0.0017;
-    const pcstFee = 0.0003;
-    const cakeFee = 0.0005;
+const feeObject = (tx, efxPcsLpRatio = new BN('0.98')) => {
+    const totalFee = new BN('25'); // 0.25%
+    const lpFee = new BN('17'); // 0.17%
+    const pcstFee = new BN('3'); // 0.03%
+    const cakeFee = new BN('5'); // 0.05%
+    const divider = new BN('10000') // divide by 1000 to get the right precision
 
     let txFeeResponse = {
         transactionHash: null,
@@ -22,26 +26,26 @@ const feeObject = (tx, efxPcsLpRatio = 0.98) => {
         cakeFee: null,
         swapFrom: null, // WBNB or EFX
         swapTo: null, // EFX or WBNB
-        swapAmountFrom: null,
-        swapAmountTo: null,
-        totalFeeFormatted: 0,
-        lpFeeFormatted: 0,
-        efxFeesFormatted: 0,
-        wbnbFeesFormatted: 0,
+        swapAmountFrom: new BN('0'),
+        swapAmountTo: new BN('0'),
+        totalFeeFormatted: new BN('0'),
+        lpFeeFormatted: new BN('0'),
+        efxFeesFormatted: new BN('0'),
+        wbnbFeesFormatted: new BN('0'),
     }
 
     if (tx.returnValues.amount0In != "0") {
         txFeeResponse.swapFrom = 'WBNB'
         txFeeResponse.swapTo = 'EFX'
-        txFeeResponse.swapAmountFrom = tx.returnValues.amount0In
-        txFeeResponse.swapAmountTo = tx.returnValues.amount1Out
+        txFeeResponse.swapAmountFrom = new BN(tx.returnValues.amount0In)
+        txFeeResponse.swapAmountTo = new BN(tx.returnValues.amount1Out)
     }
 
     if (tx.returnValues.amount1In != "0") {
         txFeeResponse.swapFrom = 'EFX'
         txFeeResponse.swapTo = 'WBNB'
-        txFeeResponse.swapAmountFrom = tx.returnValues.amount1In
-        txFeeResponse.swapAmountTo = tx.returnValues.amount0Out
+        txFeeResponse.swapAmountFrom = new BN(tx.returnValues.amount1In)
+        txFeeResponse.swapAmountTo = new BN(tx.returnValues.amount0Out)
     }
 
     // check if sender is the same as to address
@@ -49,32 +53,31 @@ const feeObject = (tx, efxPcsLpRatio = 0.98) => {
     //     console.error(`The sender is not the same as the to address: \n${JSON.stringify(tx, null, 2)}`)
     // }
     
-    txFeeResponse.transactionHash = tx.transactionHash
-    txFeeResponse.address = tx.returnValues.sender
-    txFeeResponse.totalFee = totalFee * txFeeResponse.swapAmountFrom
-    txFeeResponse.lpFee = lpFee * txFeeResponse.swapAmountFrom * efxPcsLpRatio
-    txFeeResponse.pcstFee = pcstFee * txFeeResponse.swapAmountFrom
-    txFeeResponse.cakeFee = cakeFee * txFeeResponse.swapAmountFrom
-    txFeeResponse.totalFeeFormatted = Web3.utils.fromWei(new BN(txFeeResponse.totalFee.toString()), 'ether')
-    txFeeResponse.totalFeeFormatted = Web3.utils.fromWei(new BN(txFeeResponse.lpFee.toString()), 'ether')
+    txFeeResponse.transactionHash   = tx.transactionHash
+    txFeeResponse.address           = tx.returnValues.sender
+    txFeeResponse.totalFee          = totalFee.mul(txFeeResponse.swapAmountFrom).div(divider)
+    txFeeResponse.lpFee             = lpFee.mul(txFeeResponse.swapAmountFrom.mul(efxPcsLpRatio)).div(divider)
+    txFeeResponse.pcstFee           = pcstFee.mul(txFeeResponse.swapAmountFrom).div(divider)
+    txFeeResponse.cakeFee           = cakeFee.mul(txFeeResponse.swapAmountFrom).div(divider)
+    // txFeeResponse.totalFeeFormatted = txFeeResponse.totalFee
+    // txFeeResponse.lpFeeFormatted    = txFeeResponse.lpFee
 
     if (txFeeResponse.swapFrom === 'WBNB') {
-        txFeeResponse.wbnbFeesFormatted = txFeeResponse.totalFeeFormatted
+        txFeeResponse.wbnbFeesFormatted = txFeeResponse.totalFee
     } else {
-        txFeeResponse.efxFeesFormatted = txFeeResponse.totalFeeFormatted
+        txFeeResponse.efxFeesFormatted = txFeeResponse.totalFee
     }
 
     return txFeeResponse
 }
 
-const buildList = (data) => {
-    const calculatedFees = data.map(tx => feeObject(tx))
-    return calculatedFees
-}
-
 const buildSummary = (data) => {
-    const totalEFXFees = data.reduce((a, b) => a + parseFloat(b.efxFeesFormatted), 0)
-    const totalWBNBFees = data.reduce((a, b) => a + parseFloat(b.wbnbFeesFormatted), 0)
+    
+    const totalEFXFees = data.reduce((a, b) => a.add((b.efxFeesFormatted)), new BN('0'))
+    // const totalEFXFees = data.reduce((a, b) => b.efxFeesFormatted.add(a), new BN('0'))
+    const totalWBNBFees = data.reduce((a, b) => a.add(b.wbnbFeesFormatted), new BN('0'))
+
+    console.log(`Total EFX fees: ${totalEFXFees}\nTotal WBNB fees: ${totalWBNBFees}`)
     
     const totalEfxSwaps = data.filter(tx => tx.swapFrom === 'EFX').length
     const totalWbnbSwaps = data.filter(tx => tx.swapFrom === 'WBNB').length
@@ -84,24 +87,26 @@ const buildSummary = (data) => {
     const averageWBNBFeesPerSwap = totalWBNBFees / totalWbnbSwaps
 
     // const minSwapEfx = data.reduce((a, b) => Math.min(a, b.efxFeesFormatted), Infinity)
-    const maxSwapEfx = data.reduce((a, b) => Math.max(a, b.efxFeesFormatted), 0)
+    // const maxSwapEfx = data.reduce((a, b) => BN.max(a.efxFeesFormatted, b.efxFeesFormatted))
     
     // const minSwapWBNB = data.reduce((a, b) => Math.min(a, b.wbnbFeesFormatted), Infinity)
-    const maxSwapWBNB = data.reduce((a, b) => Math.max(a, b.wbnbFeesFormatted), 0)
+    // const maxSwapWBNB = data.reduce((a, b) => BN.max(a.wbnbFeesFormatted, b.wbnbFeesFormatted))
 
     return {
         totalEFXFees: totalEFXFees,
+        efxFormatted: formatFee(totalEFXFees),
         totalWBNBFees: totalWBNBFees,
+        wbnbFormatted: formatFee(totalWBNBFees),
         totalSwaps: totalSwaps,
         totalEfxSwaps: totalEfxSwaps,
+        totalWbnbSwaps: totalWbnbSwaps,
         efxPercentage: Math.round(totalEfxSwaps / totalSwaps * 100),
         wbnbPercentage: Math.round(totalWbnbSwaps / totalSwaps * 100),
-        totalWbnbSwaps: totalWbnbSwaps,
         averageEFXFeesPerSwap: averageEFXFeesPerSwap,
         averageWBNBFeesPerSwap: averageWBNBFeesPerSwap,
-        maxSwapEfx: maxSwapEfx,
+        // maxSwapEfx: maxSwapEfx,
         // minSwapEfx: minSwapEfx,
-        maxSwapWBNB: maxSwapWBNB,
+        // maxSwapWBNB: maxSwapWBNB,
         // minSwapWBNB: minSwapWBNB,
     }
 }
