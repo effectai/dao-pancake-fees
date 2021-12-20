22ce7f61d3e09e4c7476aa8e9780b5403d1859b3
David Britt <david@effect.ai>
2021-12-20 13:27:14 +0100

    Update HTML

--- src/util.js
+++ src/util.js
@@ -1,27 +1,13 @@
 const fs = require('fs')
 const path = require('path')
-const papaparse = require('papaparse')
 
-const writeToDisk = (name, argv, data) => {
-    if (argv.json) {
-        try {
-            const jsonFileName = `${name}_${Date.now()}.json`
-            fs.writeFileSync(path.join(__dirname, `../data/${jsonFileName}`), JSON.stringify(data, null, 2))
-            console.log(`📄 Saved to ${jsonFileName}`)
-        } catch (error) {
-            console.error()
-        }
-    }
-
-    if (argv.csv) {
-        const csvFileName = `${name}_${Date.now()}.csv`
-        try {
-            const csv = papaparse.unparse(data)
-            fs.writeFileSync(path.join(__dirname, `../data/${csvFileName}`), csv)
-            console.log(`📄 Saved to ${csvFileName}`)
-        } catch (error) {
-            console.error(error)
-        }
+const writeToDisk = (name, data) => {
+    try {
+        const jsonFileName = `${name}_${Date.now()}.json`
+        fs.writeFileSync(path.join(__dirname, `../data/${jsonFileName}`), JSON.stringify(data, null, 2))
+        console.log(`📄 Saved to ${jsonFileName}`)
+    } catch (error) {
+        console.error()
     }
 }
 
@@ -56,41 +42,82 @@ const createHTML = (data) => {
   <body>
     <img id="logo" src="https://effect.network/_nuxt/img/effect-dao_h100.62b6528.png" />
 
-    <p>The Effect Network Foundation donates it's fees earned from the EFX
-    PancakeSwap pool to EffectDAO. In total <b>43044 EFX</b> has been sent
-    to the <a href="https://bloks.io/account/feepool.efx">DAO feepool</a> of
-    cycle 16. See the below table for details.</p>
+    <p>The Effect Network Foundation transfers it's fees earned from the EFX
+    PancakeSwap pool to EffectDAO. It is sent
+    to the <a href="https://bloks.io/account/feepool.efx">DAO feepool</a>. See the table below for details.</p>
 
     <table>
         <thead>
           <tr><th colspan="2">PancakeSwap Fees Summary</th></tr>
         </thead>
         <tbody>
-            <tr><td>DAO Cycle</td><td>1 to 15</td></tr>
+
+            <tr>
+                <td>Latest Update</td>
+                <td>${data.latestUpdateDateTime}</td>
+            </tr>
+
             <tr>
                 <td>Total swaps</td>
                 <td>${data.totalSwaps}</td>
             </tr>
-            <tr>
-                <td>Collected EFX fees</td>
-                <td>${parseFloat(data.inputEFX).toFixed(2)}</td>
-            </tr>
-            <tr>
-                <td>Collected BNB fees</td>
-                <td>${parseFloat(data.inputWBNB).toFixed(2)}</td>
-            </tr>
+
             <tr>
                 <td><b>Total fees for DAO in EFX</b></td>
-                <td><b>${parseFloat(data.totalCalculatedToEFX).toFixed(2)}</b></td>
+                <td><b>${parseFloat(data.foundationTotal_EFX).toFixed(2)}</b></td>
             </tr>
+
             <tr>
-                <td><i>Avg EFX Fee/Swap</i></td>
-                <td>${parseFloat(data.averageEFXFeesPerSwap).toFixed(2)}</td>
+                <td><b>Total EFX Swapped</b></td>
+                <td><b>${parseFloat(data.totalTransactions_EFX).toFixed(2)}</b></td>
             </tr>
+
             <tr>
-                <td><i>Avg BNB Fee/Swap</i></td>
-                <td>${parseFloat(data.averageWBNBFeesPerSwap).toFixed(4)}</td>
+                <td><i>Total Swaps</i></td>
+                <td>${data.totalSwaps}</td>
             </tr>
+
+            <tr>
+                <td><i>Average EFX per Swap</i></td>
+                <td>${parseFloat(data.averageEfxFeePerSwap).toFixed(2)}</td>
+            </tr>
+
+            <tr>
+                <td><i>Average Ratio</i></td>
+                <td>${parseInt(data.averageRatio)}</td>
+            </tr>
+            
+            <tr>
+                <td><i>Start Block</i></td>
+                <td><a href="https://bscscan.com/block/${data.startBlock}">${data.startBlock}</a></td>
+            </tr>
+
+            <tr>
+                <td><i>Start Block TimeStamp</i></td>
+                <td>${data.startBlockDateTime}</td>
+            </tr>
+
+
+            <tr>
+                <td><i>End Block</i></td>
+                <td><a href="https://bscscan.com/block/${data.endBlock}">${data.endBlock}</a></td>
+            </tr>
+
+            <tr>
+                <td><i>End Block TimeStamp</i></td>
+                <td>${data.endBlockDateTime}</td>
+            </tr>
+
+            <tr>
+                <td><i>Foundation Balance (Cake-LP)</i></td>
+                <td>${parseFloat(data.foundationBalanceEnd_CakeLP).toFixed(2)}</td>
+            </tr>
+
+            <tr>
+                <td><i>Total Supply (Cake-LP)</i></td>
+                <td>${parseFloat(data.totalSupplyEnd_CakeLP).toFixed(2)}</td>
+            </tr>            
+
         </tbody>
     </table>
     <p><a href="https://github.com/effectai/dao-pancake-fees">view source</a></p>
