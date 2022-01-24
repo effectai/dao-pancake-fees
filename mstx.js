8e79c8c47f75751a2717d0e4bc710dd24a067b73
David Britt <david@effect.ai>
2022-01-24 14:16:27 +0100

    tst

--- mstx.js
+++ mstx.js
@@ -56,7 +56,7 @@ const expireTransaction = (hours) => {
     return expire.toISOString().slice(0, -5); // remove milliseconds and 'Z' from ISO string
 }
 
-const transactionName = 'pancakzsfee' //name for the tx
+const transactionName = 'pancakbsfee' //name for the tx
 
 // Create proposal for transfer from bsc.efx -> feepool.efx
 const actions = [{
